ca093a1b01efde13662b2cb17f213c04be5b7e93
Daniel Lucas <me@daniellucas.net>
2015-07-23 04:07:37 -0300

    uploading the app

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {
+    "twitter": {
+        "consumer_key": 'MsXPq22YX8czKTws5ar8KcCKi',
+        "consumer_secret": 'MfzVZ7jqZam6IaNOxMxZl0Xo2uHNSuYWJSTIjGnm41Vky2ZsBp',
+        "access_token_key": '3309814888-RlVQbNjDLBFH0FAnEqGPGps9AMtDFYIcf1HlcvM',
+        "access_token_secret": 'sMqybg5W5Ednbqd3Q8JH2ZmyfMYjY2Bnv8m8F9iJq04ac'
+    },
+    "hashtags": '#GreenHug, #CPRecife4',
+    "mongo_collection": 'tweets-cprecife4',
+    "mongo_db_name": 'greenHug-bigdata'
+}
+
+module.exports = config;
